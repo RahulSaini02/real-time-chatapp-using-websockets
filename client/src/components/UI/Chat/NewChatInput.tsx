b80470e2d4c7806704f53a37efba1d40c490c386
Sateesh Kashiraju <sateeshkashiraju1001@gmail.com>
2025-03-27 01:33:57 -0500

    CHT-22 Newchat Component Created fake

--- client/src/components/UI/Chat/NewChatInput.tsx
+++ client/src/components/UI/Chat/NewChatInput.tsx
@@ -22,7 +22,7 @@ export const NewChatInput = () => {
                             </h1>
                             <div className='relative'>
                                 <input 
-                                    className="bg-white w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-fuchsia-300 text-accent outline-none placeholder:pl-2 placeholder:text-accent/70 placeholder:font-semibold caret-fuchsia-300"
+                                    className="bg-white w-full px-14 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-fuchsia-300 text-accent outline-none placeholder:pl-2 placeholder:text-accent/70 placeholder:font-semibold caret-fuchsia-300"
                                     type="text"
                                      />
                                 <FaSearch className="absolute top-1/2 left-6 transform -translate-y-1/2 text-gray-400" />
